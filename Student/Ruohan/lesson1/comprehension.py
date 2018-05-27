9dff9379f2db1599eaa63c5826ba9f8869870656
Ruohan <tangtangruohan@hotmail.com>
2018-05-26 21:31:16 -0700

    lesson01

--- Student/Ruohan/lesson1/comprehension.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-music = pd.read_csv('featuresdf.csv')
-
-gimme = music[music.danceability > 0.8]
-gimme = gimme[music.loudness < -0.5]
-gimme.sort_values(by = 'danceability', ascending = False)
-gimme = gimme[['name', 'artists']]
-
-print(gimme)
