fae5b641cc5554fe8116c9e6ba972d498d64599d
Wesley Wang <wes82@uw.edu>
2018-06-18 19:23:12 -0700

    Lesson 5 completed

--- Student/WesWang/Lesson5/Assignment/simple.py
+++ Student/WesWang/Lesson5/Assignment/simple.py
@@ -1,21 +1,33 @@
+#simple.py
 import logging
+import logging.handlers
+import datetime
 
 format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+syslog_format = "%(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 formatter = logging.Formatter(format)
+syslog_formatter = logging.Formatter(syslog_format)
 
-file_handler = logging.FileHandler('mylog.log')
+
+file_handler = logging.FileHandler(f'{timestamp}.log')
 file_handler.setLevel(logging.WARNING)           
 file_handler.setFormatter(formatter)
 
 console_handler = logging.StreamHandler()        
 console_handler.setLevel(logging.DEBUG)          
-console_handler.setFormatter(formatter)          
+console_handler.setFormatter(formatter)       
+
+syslog_handler = logging.handlers.DatagramHandler('127.0.0.1', 514)
+syslog_handler.setLevel(logging.ERROR)   
+syslog_handler.setFormatter(syslog_formatter) 
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)                   
 logger.addHandler(file_handler)
-logger.addHandler(console_handler)               
+logger.addHandler(console_handler)   
+logger.addHandler(syslog_handler)             
 
 def my_fun(n):
     for i in range(0, n):
