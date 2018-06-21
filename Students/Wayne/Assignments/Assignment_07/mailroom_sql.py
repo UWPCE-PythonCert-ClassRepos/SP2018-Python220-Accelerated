416c97405b3196a8939a78f13d1d56f87e39d472
Wayne Olson <weepler@Wayne-Olsons-MacBook-Pro.local>
2018-06-20 23:20:43 -0700

    started working on adding a donor into the database. sqlite queries written...still need to add into to applications menu

--- Students/Wayne/Assignments/Assignment_07/mailroom_sql.py
+++ Students/Wayne/Assignments/Assignment_07/mailroom_sql.py
@@ -1,317 +1,335 @@
+#!/usr/bin/env python
 """
-Simple mailroom program for a non profit
+This is an object oriented version
 """
 
-# Start with interactive loop
-
+import sqlite3
 import sys
 import math
-
 from textwrap import dedent
 
 
-"""
-The following section of code pertain to the building and manipulation of the
-donors database.
-"""
-
-# ----------------------------------------------------------------------------
-#
-# Built a sample database and put it into a dict format
-#
-# ----------------------------------------------------------------------------
-
-
-def find_donor_db():
-    return{'dave grohl': ("Dave Grohl", [999999.99, 45.63]),
-           'billy joe armstrong': ("Billy Joe Armstrong", [15000, 2222]),
-           'rivers cuomo': ("Rivers Cuomo", [1994.96, 2100000]),
-           }
-
-# ----------------------------------------------------------------------------
-#
-# Building donor class that handles all items pertaining to storing a donor
-#
-# ----------------------------------------------------------------------------
+# Utility so we have data to test with, etc.
+# def get_sample_data():
+#     """
+#     returns a list of donor objects to use as sample data
+#     """
+#     return [Donor("William Gates III", [653772.32, 12.17]),
+#             Donor("Jeff Bezos", [877.33]),
+#             Donor("Paul Allen", [663.23, 43.87, 1.32]),
+#             Donor("Mark Zuckerberg", [1663.23, 4300.87, 10432.0]),
+#             ]
 
 
 class Donor():
-    def __init__(self, name, donations=None):
-        self.uni_name = self.universal_name(name)
-        self.name = name.strip()
-        self.donations = [] if donations is None else list(donations)
+    """
+    class to hold the information about a single donor
+    """
 
-# Used to return a key
+    def __init__(self, name, donations=None):
+        """
+        create a new Donor object
+        :param name: the full name of the donor
+        :param donations=None: iterable of past donations
+        """
+
+        self.norm_name = self.normalize_name(name)
+        self.name = name.strip()
+        if donations is None:
+            self.donations = []
+        else:
+            self.donations = list(donations)
 
     @staticmethod
-    def universal_name(name):
+    def normalize_name(name):
+        """
+        return a normalized version of a name to use as a comparison key
+        simple enough to not be in a method now, but maybe you'd want to make it fancier later.
+        """
         return name.lower().strip().replace(" ", "")
 
-    # @property
-    # def donations(self):
-    #     return self._donations
-
-    def add_donation(self, amount):
-        amount = float(amount)
-        if amount <= 0.0:
-            raise ValueError("Donation must be greater than zero")
-        self.donations.append(amount)
+    @property
+    def last_donation(self):
+        """
+        The most recent donation made
+        """
+        try:
+            return self.donations[-1]
+        except IndexError:
+            return None
 
     @property
     def total_donations(self):
-        return sum(self.donations)
+        return sum(self.donations) #/ query 
 
     @property
     def average_donation(self):
         return self.total_donations / len(self.donations)
 
-    @property
-    def last_donation(self):
-        return self.donations[-1]
-
-# ----------------------------------------------------------------------------
-#
-# Building db class that handles all items pertaining to adding and storing
-# donor information
-#
-# ----------------------------------------------------------------------------
+    def add_donation(self, amount):
+        """
+        add a new donation
+        """
+        amount = float(amount) #<-- change here? 
+        if amount <= 0.0:
+            raise ValueError("Donation must be greater than zero")
+        self.donations.append(amount)
 
 
-class DonorData():
+class DonorDB():
+    """
+    encapsulation of the entire database of donors and data associated with them.
+    """
+
     def __init__(self, donors=None):
+        """
+        Initialize a new donor database
+        :param donors=None: iterable of Donor objects
+        """
         if donors is None:
             self.donor_data = {}
         else:
-            self.donor_data = {Donor.universal_name: d for d in donors}
+            self.donor_data = {d.norm_name: d for d in donors}
+
+    # def save_to_file(self, filename):
+    #     with open(filename, 'w') as outfile:
+    #         self.to_json(outfile)
+
+    # @classmethod
+    # def load_from_file(cls, filename):
+    #     with open(filename, 'r') as infile:
+    #         obj = js.from_json(infile)
+    #     return obj
 
     @property
     def donors(self):
+        """
+        an iterable of all the donors
+        """
         return self.donor_data.values()
 
-    def add_donor(self, name):
-        donor = Donor(name)
-        self.donor_data[Donor.universal_name] = donor
-        return donor
-
-    # def get_total_from_donor(self, donor_name):
-    #     return self.donors[donor_name.lower()].total_donations
-
-    def list_donors(self):
-        listing = ["Donors:"]
+    def list_donors(self):  #<---- select * from donors 
+        """
+        creates a list of the donors as a string, so they can be printed
+        Not calling print from here makes it more flexible and easier to
+        test
+        """
+        listing = ["Donor list:"]
         for donor in self.donors:
             listing.append(donor.name)
         return "\n".join(listing)
 
-    def search_donor_db(self, name):
-        return self.donor_data.get(Donor.universal_name(name))
-    #     # return donor_db.get(key)
+    def find_donor(self, name):
+        """
+        find a donor in the donor db
+        :param: the name of the donor
+        :returns: The donor data structure -- None if not in the self.donor_data
+        """
+        return self.donor_data.get(Donor.normalize_name(name))
+
+    def add_donor(self, name):
+        """
+        Add a new donor to the donor db
+        :param: the name of the donor
+        :returns: the new Donor data structure
+        """
+        # donor = Donor(name)
+        # self.donor_data[donor.norm_name] = donor
+        # return donor
+
+        donorsql - donor.db
+        table_name = donor
+        id_column = donor_id
+        name_column = donor_name
+        city_column = city
+        phonenumber_column = phone_number
+
+        #connecting to the database file 
+
+        conn = sqlite3.connect(sqlite_file)
+        c = conn.cursor()
+
+        try:
+            c.execute("INSERT INTO {tn} ({idf}, {idc}, {nc}, {cc}, {pnc}) VALUES (99999, 'test','seattle','555555555')".\
+                format(tn=table_name, idf=id_column, nc=name_column, cc=city_column, pnc= phonenumber_column))
+        except sqlite3.IntegrityError:
+            print('ERROR: ID already exists in PRIMARY KEY column {}'.format(id_column))
+
+# B) Tries to insert an ID (if it does not exist yet) <----- may be over kill 
+# with a specific value in a second column
+        # c.execute("INSERT OR IGNORE INTO {tn} ({idf}, {idc}, {nc}, {cc}, {pnc}) VALUES (99999, 'test','seattle','555555555')".\
+        #     format(tn=table_name, idf=id_column, nc=name_column, cc=city_column, pnc= phonenumber_column) 
+
+# # # C) Updates the newly inserted or pre-existing entry use <-------- for updating 
+# #         c.execute("UPDATE {tn} ({idf}, {idc}, {nc}, {cc}, {pnc}) VALUES (99999, 'test','seattle','555555555')".\
+#                 format(tn=table_name, idf=id_column, nc=name_column, cc=city_column, pnc= phonenumber_column))
+
+        conn.commit()
+        conn.close()
+
+
+
+    def gen_letter(self, donor):
+        """
+        Generate a thank you letter for the donor
+        :param: donor tuple
+        :returns: string with letter
+        note: This doesn't actually write to a file -- that's a separate
+              function. This makes it more flexible and easier to test.
+        """
+        return dedent('''Dear {0:s},
+              Thank you for your very kind donation of ${1:.2f}.
+              It will be put to very good use.
+                             Sincerely,
+                                -The Team
+              '''.format(donor.name, donor.last_donation)
+                      )
 
     @staticmethod
     def sort_key(item):
+        # used to sort on name in self.donor_data
         return item[1]
 
-    # @property
-    # def num_donors(self):
-    #     return len(donors)
-
-    # ----------------------------------------------------------------------------
-#
-# Builds the donor report and displays the name, total given, number of gifts
-# and the average gift donated by the donor.
-#
-# ----------------------------------------------------------------------------
-
-    def donor_report(self):
-
+    def generate_donor_report(self):
+        """
+        Generate the report of the donors and amounts donated.
+        :returns: the donor report as a string.
+        """
+        # First, reduce the raw data into a summary list view
         report_rows = []
         for donor in self.donor_data.values():
             name = donor.name
-            donations = donor.donations
-            total_donations = donor.total_donations
-            num_donations = len(donations)
-            avg_donation = donor.average_donation
-            report_rows.append((name, total_donations, num_donations, avg_donation))
+            gifts = donor.donations
+            total_gifts = donor.total_donations
+            num_gifts = len(gifts)
+            avg_gift = donor.average_donation
+            report_rows.append((name, total_gifts, num_gifts, avg_gift))
 
+        # sort the report data
         report_rows.sort(key=self.sort_key)
         report = []
         report.append("{:25s} | {:11s} | {:9s} | {:12s}".format("Donor Name",
-                                                                "Total Donated",
-                                                                "Num gifts",
-                                                                "Average Donation"))
-        report.append("_" * 66)
+                                                                "Total Given",
+                                                                "Num Gifts",
+                                                                "Average Gift"))
+        report.append("-" * 66)
         for row in report_rows:
-            report.append("{:25s}   ${:10.2f}   ${:9d}   ${:11.2f}".format(*row))
+            report.append("{:25s}   ${:10.2f}   {:9d}   ${:11.2f}".format(*row))
         return "\n".join(report)
 
-# ----------------------------------------------------------------------------
-#
-# Makes a standardized letter template that passes through the donor name and
-# and the amount donated
-#
-# ----------------------------------------------------------------------------
-
-    def write_letter(self, donor):
-
-        return dedent('''Dear {0:s},
-
-            Thank you for your kind doation of ${1:.2f}.
-            We will make sure that it is put to very good use.
-
-                                    Sincerely,
-                                        - The American Cancer Society)
-        '''.format(donor.name, donor.last_donation)
-                     )
-
-# ----------------------------------------------------------------------------
-#
-# Save letters to disk
-#
-# ----------------------------------------------------------------------------
-
     def save_letters_to_disk(self):
+        """
+        make a letter for each donor, and save it to disk.
+        """
         for donor in self.donor_data.values():
-            print("creating letter to:", donor.name)
-            letter = self.write_letter(donor)
+            print("Writing a letter to:", donor.name)
+            letter = self.gen_letter(donor)
+            # I don't like spaces in filenames...
             filename = donor.name.replace(" ", "_") + ".txt"
             open(filename, 'w').write(letter)
 
 
-
-# def object_oriented_db():
-#     db = DonorData()
-#     raw_data = find_donor_db()
-
-#     for k, v in raw_data.items():
-#         print(k,v)
-#         donor = Donor(k)
-#         for donation in v[1]:
-#             donor.add_donation(donation)
-#     return db
+# User-interaction code
+# Above this is all the logic code
+#  The stuff you'd need if you had a totally different UI.different
+#  below is code only for the command line interface.
 
 
-"""
-The following section of the code pertaions to the operational side of the
-"""
+# import sys
+# import math
 
-# ----------------------------------------------------------------------------
-#
-# Mainloop
-#
-# ----------------------------------------------------------------------------
+# # handy utility to make pretty printing easier
+# from textwrap import dedent
 
-testdb = DonorData(find_donor_db())
+# from mailroom import model
 
-
-def mainloop():
-
-    mainmenuinput = input(dedent("""
-        What would you like to do?
-
-        (1) - Send a Thank You Letter
-        (2) - Build a Report
-        (3) - Send letters to all donors
-        (4) - Quit
-
-        > """))
-    return mainmenuinput.strip()
+# create a DB with the sample data
+# db = DonorDB(get_sample_data())
 
 
-# def accept_donation(self, name):
-#     while True:
-#         donation_msg = input("Enter your desired donation amount"
-#                              " or 'menu' to exit)>").strip()
-#         if donation_msg == "menu":
-#             return
-#         else:
-#             donation_amt = float(donation_msg)
-#             break
-
-#     donor = self.search_donor_db(name)
-#     if donor is None:
-#         donor = testdb.add_new_donor(name)
-
-#         donor[1].append(donation_amt)
-
-#         print(thank_you_message())
+def main_menu_selection():
+    """
+    Print out the main application menu and then read the user input.
+    """
+    action = input(dedent('''
+      Choose an action:
+      1 - Send a Thank You
+      2 - Create a Report
+      3 - Send letters to everyone
+      4 - Quit
+      > '''))
+    return action.strip()
 
 
-def thank_you_message():
+def send_thank_you():
+    """
+    Record a donation and generate a thank you message.
+    """
+    # Read a valid donor to send a thank you from, handling special commands to
+    # let the user navigate as defined.
     while True:
-        name = input("Enter the donor's name or a list of donor's"
-                     "names to see all donors (or type 'menu'"
-                     "to exit)>").strip()
-        if name == 'list':
-            print(testdb.list_donors())
-        elif name == 'menu':
+        name = input("Enter a donor's name"
+                     "(or 'list' to see all donors or 'menu' to exit)> ").strip()
+        if name == "list":
+            print(db.list_donors())
+        elif name == "menu":
             return
         else:
             break
 
-# creates loop for donation amount input
-
+    # Now prompt the user for a donation amount to apply. Since this is
+    # also an exit point to the main menu, we want to make sure this is
+    # done before mutating the db.
     while True:
-        donationinput = input("Enter the amount donated "
-                              "(or 'menu' to exit)>").strip()
-        if donationinput == 'menu':
+        amount_str = input("Enter a donation amount (or 'menu' to exit)> ").strip()
+        if amount_str == "menu":
             return
-
+        # Make sure amount is a valid amount before leaving the input loop
         try:
-            amount = float(donationinput)
-            if math.isnan(amount):
+            amount = float(amount_str)
+            # extra check here -- unlikely that someone will type "NaN", but
+            # it IS possible, and it is a valid floating point number:
+            # http://en.wikipedia.org/wiki/NaN
+            if math.isnan(amount) or math.isinf(amount) or round(amount, 2) == 0.00:
                 raise ValueError
+        # in this case, the ValueError could be raised by the float() call, or by the NaN-check
         except ValueError:
-            print("please enter a valide donation amount")
+            print("error: donation amount is invalid\n")
         else:
             break
 
-    donor = testdb.search_donor_db(name)
+    # If this is a new user, ensure that the database has the necessary
+    # data structure.
+    donor = db.find_donor(name)
     if donor is None:
-        donor = testdb.add_donor(name)
+        donor = db.add_donor(name)
 
-        donor.add_donation(amount)
-
-        print(testdb.write_letter(donor))
-# ----------------------------------------------------------------------------
-#
-# Prints the donor report
-#
-# ----------------------------------------------------------------------------
+    # Record the donation
+    donor.add_donation(amount)
+    print(db.gen_letter(donor))
 
 
 def print_donor_report():
-    print(testdb.donor_report())
-
-
-# ----------------------------------------------------------------------------
-#
-# quits the program when prompted in the menu
-#
-# ----------------------------------------------------------------------------
+    print(db.generate_donor_report())
 
 
 def quit():
-    sys.exit()
-
-# ----------------------------------------------------------------------------
-#
-# Creates the main function for the Mailroom2.py exercise
-#
-# ----------------------------------------------------------------------------
+    sys.exit(0)
 
 
-def runmain():
-    mainlist_dict = {"1": thank_you_message,
-                     "2": print_donor_report,
-                     "3": testdb.save_letters_to_disk,
-                     "4": quit}
+def main():
+    selection_dict = {"1": send_thank_you,
+                      "2": print_donor_report,
+                      "3": db.save_letters_to_disk,
+                      "4": quit}
 
     while True:
-        action = mainloop()
+        selection = main_menu_selection()
         try:
-            mainlist_dict[action]()
+            selection_dict[selection]()
         except KeyError:
             print("error: menu selection is invalid!")
 
-
 if __name__ == "__main__":
-    runmain()
+
+    main()
