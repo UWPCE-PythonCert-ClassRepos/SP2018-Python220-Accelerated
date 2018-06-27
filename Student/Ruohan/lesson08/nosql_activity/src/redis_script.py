a6f6d8efaae6a0bcef104c5c732dccc708226acf
Ruohan <tangtangruohan@hotmail.com>
2018-06-26 22:22:26 -0700

    activity08

---
+++ Student/Ruohan/lesson08/nosql_activity/src/redis_script.py
@@ -0,0 +1,71 @@
+"""
+    demonstrate use of Redis
+"""
+
+
+import login_database
+import utilities
+
+
+def run_example():
+    """
+        uses non-presistent Redis only (as a cache)
+
+    """
+
+    log = utilities.configure_logger('default', '../logs/redis_script.log')
+
+    try:
+        log.info('Step 1: connect to Redis')
+        r = login_database.login_redis_cloud()
+        log.info('Step 2: cache some data in Redis')
+        r.set('andy', 'andy@somewhere.com')
+
+        log.info('Step 2: now I can read it')
+        email = r.get('andy')
+        log.info('But I must know the key')
+        log.info(f'The results of r.get: {email}')
+
+        log.info('Step 3: cache more data in Redis')
+        r.set('pam', 'pam@anywhere.com')
+        r.set('fred', 'fred@fearless.com')
+
+        #Add some customer data to the cache
+        log.info('Add some customer data to the cache')
+        r.set('Cara', '209-234-9878', '98023')
+        r.set('Ellen', '234-098-1234', '98072')
+        r.set('Suki', '298-987-3456', '98098')
+        r.set('Chris', '234-976-7765', '98091')
+        r.set('Nate', '298-785-3989', '98017')
+        r.set('Katy', '240-897-6656', '98055')
+
+        #retrieve a zip code and then a phone number for a known customer
+        log.info('retrieve a zip code and then a phone number for Ellen')
+        print(f"Ellen's zipcode is {r.lindex('Ellen', 1)} and phone number is {r.lindex('Ellen', 0)}")
+
+        log.info('Step 4: delete from cache')
+        r.delete('andy')
+        log.info(f'r.delete means andy is now: {email}')
+
+        log.info(
+            'Step 6: Redis can maintain a unique ID or count very efficiently')
+        r.set('user_count', 21)
+        r.incr('user_count')
+        r.incr('user_count')
+        r.decr('user_count')
+        result = r.get('user_count')
+        log.info('I could use this to generate unique ids')
+        log.info(f'Redis says 21+1+1-1={result}')
+
+        log.info('Step 7: richer data for a SKU')
+        r.rpush('186675', 'chair')
+        r.rpush('186675', 'red')
+        r.rpush('186675', 'leather')
+        r.rpush('186675', '5.99')
+
+        log.info('Step 8: pull some data from the structure')
+        cover_type = r.lindex('186675', 2)
+        log.info(f'Type of cover = {cover_type}')
+
+    except Exception as e:
+        print(f'Redis error: {e}')
