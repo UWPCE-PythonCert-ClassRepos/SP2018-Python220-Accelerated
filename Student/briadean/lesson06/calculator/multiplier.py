9f3a8df8599729f905c7de4e96c4a6b0a711ca08
Bri Dean <Briadean@protonmail.com>
2018-07-07 18:18:32 -0700

    Completed lesson06 Activity

---
+++ Student/briadean/lesson06/calculator/multiplier.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+"""Multiplication module"""
+
+
+class Multiplier:
+    """Multiplication class"""
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """Calculation method"""
+        return operand_1 * operand_2
