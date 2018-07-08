cab03b934a4864682d15389fe084411fc473ecd5
Wesley Wang <wes82@uw.edu>
2018-07-08 13:45:24 -0700

    Lesson10 complete

--- Student/WesWang/Lesson10/Activity/great_circle.py
+++ Student/WesWang/Lesson10/Activity/great_circle.py
@@ -5,10 +5,10 @@
 #    $ /usr/bin/time -p python great_circle.py
 #    $ /usr/bin/time --verbose python great_circle.py
 
-from great_circle_v0 import great_circle
+#from great_circle_v0 import great_circle
 #from great_circle_v1 import great_circle
 #from great_circle_v2 import great_circle
-#from great_circle_v3 import great_circle
+from great_circle_v3 import great_circle
 
 lon1, lat1, lon2, lat2 = -72.345, 34.323, -61.823, 54.826
 
