6b6102bd15fce4c4965a2f791a95b3d637798470
Ryan Drovdahl <rdrovdahl@Ryans-MacBook-Pro.local>
2018-07-10 13:18:21 -0700

    adding lesson08 activity

---
+++ Student/rdrovdahl/lesson08/no_sql/src/learn_data.py
@@ -0,0 +1,69 @@
+"""
+    Data for database demonstrations
+"""
+
+
+def get_furniture_data():
+    """
+    demonstration data
+    """
+
+    furniture_data = [
+        {
+            'product_type': 'couch',
+            'product_color': 'red',
+            'description': 'Leather low back',
+            'monthly_rental_cost': 12.99,
+            'in_stock_quantity': 10
+        },
+        {
+            'product_type': 'couch',
+            'product_color': 'blue',
+            'description': 'Cloth high back',
+            'monthly_rental_cost': 9.99,
+            'in_stock_quantity': 3
+        },
+        {
+            'product_type': 'coffee table',
+            'product_color': 'white',
+            'description': 'Plastic',
+            'monthly_rental_cost': 2.50,
+            'in_stock_quantity': 25
+        },
+        {
+            'product_type': 'couch',
+            'product_color': 'red',
+            'description': 'Leather high back',
+            'monthly_rental_cost': 15.99,
+            'in_stock_quantity': 17
+        },
+        {
+            'product_type': 'recliner',
+            'product_color': 'blue',
+            'description': 'Leather high back',
+            'monthly_rental_cost': 19.99,
+            'in_stock_quantity': 6
+        },
+        {
+            'product_type': 'chair',
+            'product_color': 'black',
+            'description': 'Plastic',
+            'monthly_rental_cost': 1.00,
+            'in_stock_quantity': 45
+        },
+        {
+            'product_type': 'bunk bed',
+            'product_color': 'white',
+            'description': 'Boys twin size bunk bed',
+            'monthly_rental_cost': 5.00,
+            'in_stock_quantity': 25
+        },
+        {
+            'product_type': 'rocking chair',
+            'product_color': 'red',
+            'description': 'Grandmas favorite!',
+            'monthly_rental_cost': 2.50,
+            'in_stock_quantity': 19
+        }
+    ]
+    return furniture_data
